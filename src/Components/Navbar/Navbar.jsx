0328a824c2759284cc748b1b254c9a633c160fef
SANJARBEK HAKIMOV <sanjarbekhakimov020@gmail.com>
2024-11-15 16:13:08 +0500

    login modal qo'shildi

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,15 +12,16 @@ const Navbar = () => {
     const [isCitizen, setIsCitizen] = useState(null);
     const [phone, setPhone] = useState('');
     const [activeNav, setActiveNav] = useState(false)
+    const [citizenship, setCitizenship] = useState('uzb')
 
     const handleCitizenChange = (event) => {
         setIsCitizen(event.target.value);
     };
     const activeNavHandle = () => {
-        setActiveNav(x=>!x)
+        setActiveNav(x => !x)
     }
     const closeHandle = () => {
-        setIsModalOpen(false)
+        setIsModalOpen(x => !x)
     }
     return (
         <div className="container">
@@ -75,56 +76,43 @@ const Navbar = () => {
                                 <h1 className="login-title">Login</h1>
                                 <div onClick={closeHandle} className="close"></div>
                             </div>
-                            <form>
-                                <p className="text-check">Are you citizen of Uzbekistan?</p>
-                                <div className="radio-block">
-                                    <label className='label'>
-
-                                        <input required className='check-input' checked={isCitizen === 'yes'}
-                                            onChange={handleCitizenChange} type="radio" value={"yes"} />
-                                        <div className="radio">
-                                            <p className="radio-center"></p>
-                                        </div>Yes
-                                    </label>
-                                    <label className='label'>
-                                        <input required className='check-input' checked={isCitizen === 'no'}
-                                            onChange={handleCitizenChange} type="radio" value={"no"} />
-                                        <div className="radio">
-                                            <p className="radio-center"></p>
-                                        </div>No
-                                    </label>
+                            <div>
+                                <div className="citizenship">
+                                    <button onClick={() => setCitizenship('uzb')} className={`citizenship-btn ${citizenship === 'uzb' ? 'citizenship-active' : ''}`}>Uzb fuqorosi</button>
+                                    <button onClick={() => setCitizenship('chet')} className={`citizenship-btn ${citizenship === 'chet' ? 'citizenship-active' : ''}`}>Chet fuqorosi</button>
                                 </div>
-                                <input required className='modalLogin-input' placeholder='Name' type="text" />
-                                <input required className='modalLogin-input input' placeholder='Email' type="email" />
-                                <PhoneInput
-                                    country={'us'}
-                                    value={phone} h
-                                    onChange={(phone) => setPhone(phone)}
-                                    inputStyle={{
-                                        width: '100%',
-                                        height: '40px',
-                                        backgroundColor: '#f5f7fa',
-                                        border: '1px solid #ddd',
-                                        borderRadius: '8px',
-                                        paddingLeft: '60px',
-                                        fontSize: '16px',
-                                    }}
-                                    buttonStyle={{
-                                        border: 'none',
-                                        borderRadius: '8px 0 0 8px',
-                                        padding: '0 10px',
-                                    }}
-                                    dropdownStyle={{
-                                        borderRadius: '8px',
-                                        padding: 0,
-                                    }}
-                                />
-                                <div className="submit-block">
-                                    <button onClick={closeHandle} type='click' className="cancel">Cancel</button>
-                                    <button type='submit' className="submit btn">Confirm</button>
-                                </div>
-                                <button className='oneId'>OneId orqali kirish</button>
-                            </form>
+                                <form>
+                                    <input required className='modalLogin-input' placeholder='Name' type="text" />
+                                    <input required className='modalLogin-input input' placeholder='Email' type="email" />
+                                    <PhoneInput
+                                        country={'us'}
+                                        value={phone} h
+                                        onChange={(phone) => setPhone(phone)}
+                                        inputStyle={{
+                                            width: '100%',
+                                            height: '40px',
+                                            backgroundColor: '#f5f7fa',
+                                            border: '1px solid #ddd',
+                                            borderRadius: '8px',
+                                            paddingLeft: '60px',
+                                            fontSize: '16px',
+                                        }}
+                                        buttonStyle={{
+                                            border: 'none',
+                                            borderRadius: '8px 0 0 8px',
+                                            padding: '0 10px',
+                                        }}
+                                        dropdownStyle={{
+                                            borderRadius: '8px',
+                                            padding: 0,
+                                        }}
+                                    />
+                                    <div className="submit-block">
+                                        <button onClick={closeHandle} type='click' className="cancel">Cancel</button>
+                                        <button type='submit' className="submit btn">Confirm</button>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 )
